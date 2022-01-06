f70ee3a290407b4e27af13f0c66dff027b837812
dani <dani.zaghian@gmail.com>
2022-01-06 10:38:24 -0800

    adds inheritance example with super keyword

--- inheritance_example.rb
+++ inheritance_example.rb
@@ -1,5 +1,5 @@
 class Vehicle
-  def initialize
+  def initialize(options_hash)
     @speed = 0
     @direction = 'north'
   end
@@ -18,21 +18,39 @@ class Vehicle
 end
 
 class Car < Vehicle
+  attr_accessor :make, :model
+
+  def initialize(options_hash)
+    super
+    @make = options_hash[:make]
+    @model = options_hash[:model]
+  end
+
   def honk_horn
     puts "Beeeeeeep!"
   end
 end
 
 class Bike < Vehicle
+  attr_accessor :type, :weight
+
+  def initialize(options_hash)
+    super
+    @type = options_hash[:type]
+    @weight = options_hash[:weight]
+  end
+
   def ring_bell
     puts "Ring ring!"
   end
 end
 
-car = Car.new
+car = Car.new(make: 'Ford', model: 'F150')
 car.brake
 car.honk_horn
+puts car.make
 
-bike = Bike.new
+bike = Bike.new(type: 'Mountain', weight: 20)
 bike.accelerate
 bike.ring_bell
+puts bike.type
